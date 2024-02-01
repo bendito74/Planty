f8157cafe6cf6353c5f3c332d717692b5ef30a75
Ben FORET <foret.benjamin@laposte.net>
2024-02-01 14:41:09 +0100

    ajout de la function pour afficher le lien admin

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -7,4 +7,15 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
+/* Ajouter un lien "Admin" dans le menu de navigation pour les utilisateurs connectés*/
+function ajouter_lien_admin_menu( $items, $args ) {
+    if ( is_user_logged_in() && isset( $args->theme_location ) && $args->theme_location === 'primary' ) {
+        // Construire le lien "Admin"
+        $admin_link = '<li><a href="' . admin_url() . '" class="adminButton">Admin</a></li>';
+        // Ajouter le lien à la fin du menu
+        $items .= $admin_link;
+    }
+    return $items;
+}
+add_filter( 'wp_nav_menu_items', 'ajouter_lien_admin_menu', 10, 2 );
 ?>
